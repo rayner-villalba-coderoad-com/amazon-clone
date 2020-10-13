512beed762b62dad561c59fda744b2ce8cfc7c36
rayner-villalba-coderoad-com <rayner.villalba@coderoad.com>
2020-10-13 19:51:06 -0400

    Geeting started amazon clone add header

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBkKQXG6eVGcW0j9qC5ojES41evvrTFH2A",
+  authDomain: "challenge-488c5.firebaseapp.com",
+  databaseURL: "https://challenge-488c5.firebaseio.com",
+  projectId: "challenge-488c5",
+  storageBucket: "challenge-488c5.appspot.com",
+  messagingSenderId: "441998568748",
+  appId: "1:441998568748:web:7643b584fdcbcc54b21089",
+  measurementId: "G-6EHGDPYFBX"
+};
+  
